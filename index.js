931024c23f47b31e17c86ebeb2a517e3ac1a2b76
Riyuuu1703 <riyuuuu0317@gmail.com>
2023-05-24 12:24:23 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+let tablinks = document.getElementsByClassName('tab-links');
+let tabcontents = document.getElementsByClassName('tab-contents')
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link")
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab")
+    }
+    event.currentTarget.classList.add("active-link")  
+    document.getElementById(tabname).classList.add("active-tab")
+}
+
+var sidemenu= document.getElementById('sidemenu');
+
+function Openmenu(){
+    sidemenu.style.right="0";
+}
+function Closemenu(){
+    sidemenu.style.right="-200px";
+}
+
+
+// ----Google---
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwbAhxs6Uhs1CwNWP9gWiAUaiVtSc7bjWCeIoHrh9UEj6tgFx_7-LX84EWte9MSnOqz/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg= document.getElementById('msg')
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response =>  {
+        msg.innerHTML = "Message sent successfully!"
+        setTimeout(function(){
+            msg.innerHTML=" "
+        },5000)
+        form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
+
